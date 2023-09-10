8037b5dde00d0925e73a52a301a3d814bdb94b37
Thomy <126921685+Ywidow@users.noreply.github.com>
2023-09-10 20:52:15 -0300

    Delete Modulo3 - Copia directory

--- Modulo3 - Copia/Scripts/ContactScript.js
+++
@@ -1,8 +0,0 @@
-const contactSection = document.getElementById('Contato');
-
-const formFromContactSection = contactSection.getElementsByTagName('form');
-formFromContactSection[0].addEventListener(
-  'submit', function(){
-    localStorage.setItem("FormStatus", "Succes");
-  }
-);
